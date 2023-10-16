0bcd760e51bab18da50828761631e3a69245f86b
erikvella <vella.erik1@gmail.com>
2023-10-16 16:15:40 +0100

    [add] API

--- src/data/store.js
+++ src/data/store.js
@@ -1,5 +1,7 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
-  apiUrl:'9962f1aa715593ba3aaac38a4bb5036b'
+  apiUrl:'https://api.themoviedb.org/3/search/movie',
+  apiKey: '9962f1aa715593ba3aaac38a4bb5036b',
+  titleToSearch:''
 })
